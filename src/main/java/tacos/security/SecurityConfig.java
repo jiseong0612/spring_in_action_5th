d20a4e6989a88f6ca5014bd72e623d102c7bd508
jiseong0612 <hth5545@gmail.com>
2023-03-20 00:07:15 +0900

    p.125 JDBC기반의 사용자 스토어

---
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package tacos.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	DataSource dataSource;
+	/**
+	 * Http 보안을 구성 한다
+	 */
+	protected void configure(HttpSecurity http) throws Exception{
+		http.authorizeRequests()
+		.antMatchers("/design", "/orders")
+		.access("hasRole('ROLE_USER')")
+		.antMatchers("/", "/**").access("permitAll")
+		.and()
+		.httpBasic();
+	}
+	
+	/**
+	 * 사용자 인증 정보를 구성한다.
+	 */
+	public void configure(AuthenticationManagerBuilder auth)throws Exception{
+		auth.jdbcAuthentication()
+		.dataSource(dataSource);
+		/*
+		 * auth.inMemoryAuthentication() .withUser("user1") .password("{noop}password1")
+		 * .authorities("ROLE_USER") .and() .withUser("user2")
+		 * .password("{noop}password2") .authorities("ROLE_USER");
+		 */
+	}
+}
